95e7ffbd9eae8ae4d8714b131c537eb6400d716a
Rafi5734 <kamrulhasun5734@gmail.com>
2023-02-13 23:26:52 +0600

    41:48 upto min

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Pages/Home';
 import Cart from "./Pages/Cart";
 import Navbar from './Component/Navbar';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<Home/>}></Route>
-          <Route path="/cart" element={<Cart/>}></Route>
-       </Routes>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/cart" element={<Cart />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
